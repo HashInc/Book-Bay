704499fb4d3cb31e1e9179d9a796e6500ad45113
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-10-25 16:44:30 +0530

    1

--- checkout.php
+++ checkout.php
@@ -249,41 +249,17 @@ Smartphone Compatible web template, free webnovels for Nokia, Samsung, LG, SonyE
 			<th>Subtotal</th>
 		  </tr>
 		  <tr>
-			<td class="ring-in"><a href="single.php" class="at-in"><img src="images/ce.jpg" class="img-responsive" alt=""></a>
+			<td class="ring-in"><a href="don.php" class="at-in"><img src="images/ce.jpg" class="img-responsive" alt=""></a>
 			<div class="sed">
-				<h5>Sed ut perspiciatis unde</h5>
-				<p>(At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium) </p>
+				<h5>Death on the nile</h5>
+				<p>by Agatha Christie</p>
 			
 			</div>
 			<div class="clearfix"> </div></td>
 			<td class="check"><input type="text" value="1" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='';}"></td>		
-			<td>Rs.100.00</td>
+			<td>Rs.110.00</td>
 			<td>FREE SHIPPING</td>
-			<td>Rs.100.00</td>
-		  </tr>
-		  <tr>
-		  <td class="ring-in"><a href="single.php" class="at-in"><img src="images/ce1.jpg" class="img-responsive" alt=""></a>
-			<div class="sed">
-				<h5>Sed ut perspiciatis unde</h5>
-				<p>(At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium ) </p>
-			</div>
-			<div class="clearfix"> </div></td>
-			<td class="check"><input type="text" value="1" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='';}"></td>		
-			<td>Rs.200.00</td>
-			<td>FREE SHIPPING</td>
-			<td>Rs.200.00</td>
-		  </tr>
-		  <tr>
-		  <td class="ring-in"><a href="single.php" class="at-in"><img src="images/ce2.jpg" class="img-responsive" alt=""></a>
-			<div class="sed">
-				<h5>Sed ut perspiciatis unde</h5>
-				<p>(At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium) </p>
-			</div>
-			<div class="clearfix"> </div></td>
-			<td class="check"><input type="text" value="1" onfocus="this.value='';" onblur="if (this.value == '') {this.value ='';}"></td>		
-			<td>Rs.150.00</td>
-			<td>FREE SHIPPING</td>
-			<td>Rs.150.00</td>
+			<td>Rs.110.00</td>
 		  </tr>
 	</table>
 	<a href="#" class=" to-buy">PROCEED TO BUY</a>
